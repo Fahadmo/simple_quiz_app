9daa6f9732b0a645ce08bf320bc3719104f3eb8f
Fahad <mohammadfahad27@gmail.com>
2019-04-08 23:07:51 -0400

    Created simple quiz app

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = ["What is the power house of the cell?",
+                               "What is 10 + 2?",
+                               "What is capital of New Jersey?"]
+    let answers: [String] = ["Mitochondria",
+                             "12",
+                             "Trenton"]
+    var currentQuestionIndex: Int = 0
+    
+    @IBAction func showNextQuestion(sender: AnyObject) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnswer(sender: AnyObject) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        questionLabel.text = questions[currentQuestionIndex]
     }
-
-
 }
 
